5fc66e0a88a09d88abceb9ab636e015907156a52
K-J-HYEON <kjh950601@naver.com>
2021-11-05 17:12:17 +0900

    mini_blog

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
     "리액트 독학",
   ]);
   let [최고, 최고변경] = useState(0);
-
-  let [modal, modal변경] = useState(false);
-
+  let [modal, modal변경] = useState(true);
   let [누른제목, 누른제목변경] = useState(0);
+
+  let [입력값, 입력값변경] = useState("");
 
   function 제목바꾸기() {
     let newArray = [...글제목];
@@ -27,11 +27,10 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={제목바꾸기}>버튼</button>
-
+      {/* <button onClick={제목바꾸기}>버튼</button> */}
       {글제목.map(function (글, i) {
         return (
-          <div className="list">
+          <div className="list" key={i}>
             <h3
               onClick={() => {
                 누른제목변경(i);
@@ -54,28 +53,22 @@ function App() {
         );
       })}
 
-      {/* <button
-        onClick={() => {
-          누른제목변경(0);
-        }}
-      >
-        버튼1
-      </button>
-      <button
-        onClick={() => {
-          누른제목변경(1);
-        }}
-      >
-        버튼2
-      </button>
-      <button
-        onClick={() => {
-          누른제목변경(2);
-        }}
-      >
-        버튼3
-      </button> */}
-
+      <div className="publish">
+        <input
+          onChange={(e) => {
+            입력값변경(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            let arrayCopy = [...글제목];
+            arrayCopy.unshift(입력값);
+            글제목변경(arrayCopy);
+          }}
+        >
+          저장
+        </button>
+      </div>
       <button
         onClick={() => {
           modal변경(!modal);
@@ -93,7 +86,7 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h2> {props.글제목[props.누른제목]} </h2>
+      <h2> {props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
